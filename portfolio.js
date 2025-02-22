b82e72174ebcd6f306ef03ae689fe474779ee1a3
Ryuuen8 <sabridobryan@gmail.com>
2025-02-22 22:28:53 +0800

    Fifth Commit

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    emailjs.init("mX-q13fEtB8WZ-5Zf"); 
+    document.getElementById("contact-form").addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        let params = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            message: document.getElementById("message").value
+        };
+
+        emailjs.send("service_tepwqin", "template_s8ava8t", params)
+            .then(function (response) {
+                console.log("SUCCESS!", response);
+                alert("Email sent successfully!");
+            })
+            .catch(function (error) {
+                console.error("FAILED...", error);
+                alert("Error sending email: " + error);
+            });
+    });
+});
+
 
 $(document).ready(function() {
     $("a").on("click", function(event) {
