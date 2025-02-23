b6f5bee606cf957b7beb667febba20c32a57d8b2
Ryuuen8 <sabridobryan@gmail.com>
2025-02-23 22:47:15 +0800

    Last Day Commit

--- portfolio.js
+++ portfolio.js
@@ -22,6 +22,19 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+const gra = function(min, max) {
+    return Math.random() * (max - min) + min;
+}
+const init = function(){
+  let items = document.querySelectorAll('section');
+  for (let i = 0; i < items.length; i++){
+    items[i].style.background = randomColor({luminosity: 'light'});
+  }
+  cssScrollSnapPolyfill()
+}
+init();
+
+
 $(document).ready(function() {
     $("a").on("click", function(event) {
         if (this.hash !== "") {
